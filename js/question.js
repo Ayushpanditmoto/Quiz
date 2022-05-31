77180face572e29118d17bcb62be9a5b7eb75937
Your <you@example.com>
2022-05-31 13:54:13 +0530

    added question fixed some bugs

--- js/question.js
+++ js/question.js
@@ -32,7 +32,7 @@ const quiz = [
   {
     q: "Non animated gifs are usually lower in file size, why don't people use them for photos",
     options: [
-      "Because it hasn't caught on.",
+      "Because it hasn 't caught on.",
       'You need to pay for a lisencse to use them.',
       'They are distracting and decrease Usability.',
       'None of the above',
@@ -177,5 +177,200 @@ const quiz = [
     q: 'What is the sizeof(char) in a 32bit C compiler?',
     options: ['1 bit', '2 bits', '1 Byte', '2 Bytes'],
     answer: 2,
+  },
+  {
+    q: 'HTML stands for -',
+    options: [
+      'HighText Machine Language',
+      'HyperText and links Markup Language',
+      'HyperText Markup Language',
+      'None of these',
+    ],
+    answer: 2,
+  },
+  {
+    q: 'The correct sequence of HTML tags for starting a webpage is - ',
+    options: [
+      'Head, Title, HTML, body',
+      'HTML, Body, Title, Head',
+      'HTML, Head, Title, Body',
+      'HTML, Head, Title, Body',
+    ],
+    answer: 3,
+  },
+  {
+    q: 'Which of the following element is responsible for making the text bold in HTML?',
+    options: ['pre', 'a', 'b', 'br'],
+    answer: 2,
+  },
+  {
+    q: 'Which of the following tag is used for inserting the largest heading in HTML?',
+    options: ['h3', 'h1', 'h5', 'h6'],
+    answer: 1,
+  },
+  {
+    q: 'Which character is used to represent the closing of a tag in HTML?',
+    options: [' /', ' ?', ' .', ' !'],
+    answer: 0,
+  },
+  {
+    q: 'How to insert an image in HTML?',
+    options: [
+      'img href = "jtp.png" ',
+      'img url = "jtp.png" ',
+      'img link = "jtp.png" ',
+      'img src = "jtp.png" ',
+    ],
+    answer: 3,
+  },
+  {
+    q: '<input> is -',
+    options: [
+      'a format tag.',
+      'an empty tag.',
+      'All of the above',
+      'None of the above',
+    ],
+    answer: 1,
+  },
+  {
+    q: 'The <hr> tag in HTML is used for -',
+    options: [
+      'new line',
+      'vertical ruler',
+      'new paragraph',
+      'horizontal ruler',
+    ],
+    answer: 3,
+  },
+  {
+    q: 'Which of the following option leads to the portability and security of Java?',
+    options: [
+      'Bytecode is executed by JVM',
+      'The applet makes the Java code secure and portable',
+      'Use of exception handling',
+      'Dynamic binding between objects',
+    ],
+    answer: 0,
+  },
+  {
+    q: 'Which of the following is not a Java features?',
+    options: [
+      'Dynamic',
+      'Architecture Neutral',
+      'Use of pointers',
+      'Object-oriented',
+    ],
+    amswer: 2,
+  },
+  {
+    q: '_____ is used to find and fix bugs in the Java programs.',
+    options: ['JVM', 'JRE', 'JDK', 'JDB'],
+    answer: 3,
+  },
+  {
+    q: 'Which of the following is a valid declaration of a char?',
+    options: [
+      "char ch = 'utea';",
+      "char ca = 'tea';",
+      'char cr = \u0223;',
+      "char cc = 'itea';",
+    ],
+    answer: 0,
+  },
+  {
+    q: 'What is the return type of the hashCode() method in the Object class?',
+    options: ['Object', 'int', 'long', 'void'],
+    answer: 1,
+  },
+  {
+    q: 'Evaluate the following Java expression, if x=3, y=5, and z=100 ++z + y - y + z + x++',
+    options: ['24', '23', '20', '25'],
+    answer: 3,
+  },
+  {
+    q: 'Which of the following tool is used to generate API documentation in HTML format from doc comments in source code?',
+    options: ['javap tool', 'javaw command', 'Javadoc tool', 'javah command'],
+    answer: 2,
+  },
+  {
+    q: 'Which method of the Class.class is used to determine the name of a class represented by the class object as a String?',
+    options: ['getClass()', 'intern()', 'getName()', 'toString()'],
+    answer: 2,
+  },
+  {
+    q: 'What is C++ ?',
+    options: [
+      'C++ is an object oriented programming language',
+      'C++ is a procedural programming language',
+      'C++ supports both procedural and object oriented programming language',
+      'C++ is a functional programming language',
+    ],
+    answer: 2,
+  },
+  {
+    q: 'Which of the following is used for comments in C++?',
+    options: [
+      '/* comment */',
+      '// comment */',
+      '// comment',
+      'both // comment or /* comment */',
+    ],
+    answer: 3,
+  },
+  {
+    q: 'Which of the following user-defined header file extension used in c++?',
+    options: ['hg', 'cpp', 'h', 'hf'],
+    answer: 2,
+  },
+  {
+    q: 'Which of the following type is provided by C++ but not C?',
+    options: ['double', 'float', 'int', 'bool'],
+    answer: 3,
+  },
+  {
+    q: 'By default, all the files in C++ are opened in _________ mode.',
+    options: ['Binary', 'VTC', 'Text', 'ISCII'],
+    answer: 2,
+  },
+  {
+    q: 'Which of the following correctly declares an array in C++?',
+    options: ['array{10};', 'array array[10];', 'int array;', 'int array[10];'],
+    answer: 3,
+  },
+  {
+    q: 'What is the size of wchar_t in C++?',
+    options: ['Based on the number of bits in the system', '2 or 4', '4', '2'],
+    answer: 0,
+  },
+  {
+    q: ' Which is more effective while calling the C++ functions?',
+    options: [
+      'call by object',
+      'call by pointer',
+      'call by value',
+      'call by reference',
+    ],
+    answer: 3,
+  },
+  {
+    q: 'Which of the following is used to terminate the function declaration in C++?',
+    options: [';', ']', ')', ':'],
+    answer: 0,
+  },
+  {
+    q: 'Which keyword is used to define the macros in c++?',
+    options: ['#macro', '#define', 'macro', 'define'],
+    answer: 1,
+  },
+  {
+    q: 'What is Inheritance in C++ ?',
+    options: [
+      'Deriving new classes from existing classes',
+      'Overloading of classes',
+      'Classes with same names',
+      'Wrapping of data into a single class',
+    ],
+    answer: 0,
   },
 ]
